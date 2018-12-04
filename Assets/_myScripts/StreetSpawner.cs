820db98ed6084f51ab10379eb50d712fd3a90246
Keyairra Wright <keyairra.wright@gmail.com>
2018-12-04 12:27:08 -0500

    updating StreetSpawner script to include tile clean-up

---
+++ Assets/_myScripts/StreetSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic; //necessary to use the list types
+using UnityEngine;
+
+public class StreetSpawner : MonoBehaviour {
+
+    public GameObject[] streetTilePrefabs; //This allows the Street Tile Prebas to be eventuall spawned
+
+    private Transform playerTransform;
+    private float spawnZ = -24.0f; //this will will cause a tile to spawn behind the player so that the user's view is not empty at first
+    private float streetTileLength = 24.0f;
+    private float safeZone = 15.0f;
+    private int numTilesOnScreen = 7;//this may be adjusted based on camera view during testing
+
+    private List<GameObject> activeTiles;
+
+	void Start () {
+        activeTiles = new List<GameObject>();
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+   
+
+        for (int i = 0; i < numTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(playerTransform.position.z - safeZone > (spawnZ - numTilesOnScreen * streetTileLength)) 
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+	}
+
+    private void SpawnTile(int prefabIndex = -1) //default index for tiles to spawn
+    {
+        GameObject go;
+        go = Instantiate(streetTilePrefabs[0] as GameObject);
+        go.transform.SetParent(transform);//grabs reference to our tile being created and specifies that the parent is now the StreetManager object in the hiearchy
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += streetTileLength;
+        activeTiles.Add(go);
+    }
+
+    private void DeleteTile(){
+        Destroy (activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
